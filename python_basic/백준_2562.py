1059c7832dab3dac220e4f42bcc3d3bb0469ae91
seok jin <tjrwls08088@chungbuk.ac.kr>
2021-08-29 00:49:18 +0900

    feat: list, str

--- python_basic/백준_2562.py
+++ python_basic/백준_2562.py
@@ -1,8 +1,8 @@
 numbers = []
 
 for x in range(9):
-    i = int(input())
-    numbers.append(i)
+    x = int(input())
+    numbers.append(x)
 
 print(max(numbers))
 print(numbers.index(max(numbers))+1)
