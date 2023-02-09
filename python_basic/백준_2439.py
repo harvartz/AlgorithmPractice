52c95ae70a7bbafadfcd808a7c0b667b4cb31b7f
seokjin <tjrwls08088@naver.com>
2023-02-09 15:09:07 +0900

    Remodel Files Structure

--- python_basic/백준_2439.py
+++
@@ -1,5 +0,0 @@
-#별 반대로 찍기
-
-a = int(input())
-for i in range(1,a+1):
-    print(" " * (a-i) + "*" * i)
--- python_basic/백준_2562.py
+++
@@ -1,8 +0,0 @@
-numbers = []
-
-for x in range(9):
-    x = int(input())
-    numbers.append(x)
-
-print(max(numbers))
-print(numbers.index(max(numbers))+1)
