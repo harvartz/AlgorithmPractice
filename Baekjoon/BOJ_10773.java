294e7413e3abf3b142c396d6a089ce442bfcb8a3
seokjin <tjrwls08088@naver.com>
2023-02-12 23:14:56 +0900

    upload 10773

---
+++ Baekjoon/BOJ_10773.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+   public static void main(String args[]) {
+       Stack<Integer> stack = new Stack<Integer>();
+       Scanner sc = new Scanner(System.in);
+       int N = sc.nextInt();
+
+       for(int x = 0; x<N; x++){
+           int M = sc.nextInt();
+           if (M == 0) {
+               stack.pop();
+           }
+           else{
+               stack.push(M);
+           }
+       }
+       int sum = 0;
+
+       for(int o : stack){
+           sum += o;
+       }
+
+       System.out.println(sum);
+   }
+}
