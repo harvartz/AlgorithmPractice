c53252765dc26a415838bb5a5850f010aa390714
seokjin <tjrwls08088@naver.com>
2023-03-04 15:52:57 +0900

    upload 완주하지 못한 선수

---
+++ programmers/level1/완주하지_못한_선수.java
@@ -0,0 +1,29 @@
+import javax.sound.midi.Patch;
+import java.util.*;
+
+public class Main {
+   public static void main(String []args){
+      String[] participant = {"leo", "kiki", "eden"};
+      String[] completion = {"eden", "kiki"};
+
+      String answer = "";
+
+      HashMap<String, Integer> hashMap = new HashMap<>();
+
+      for(String p : participant){
+         hashMap.put(p, hashMap.getOrDefault(p,0)+1);
+      }
+
+      for(String c : completion){
+         hashMap.put(c, hashMap.get(c)-1);
+      }
+
+      for(String r : hashMap.keySet()){
+         if(hashMap.get(r) != 0){
+            answer = r;
+         }
+      }
+
+      System.out.println(answer);
+   }
+}
