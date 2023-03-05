8952a22e2b127c6b3d7c036d729f319bff323a2f
seokjin <tjrwls08088@naver.com>
2023-03-05 16:42:09 +0900

    upload 전화번호_목록, level2

---
+++ programmers/level2/전화번호_목록.java
@@ -0,0 +1,27 @@
+import javax.sound.midi.Patch;
+import java.util.*;
+
+public class Main {
+   public static void main(String []args){
+      String[] input = {"119", "97674223", "1195524421"};
+
+      HashMap<String, Integer> hashMap = new HashMap<>();
+
+      for(String in : input){
+         hashMap.put(in, 1);
+      }
+
+      System.out.println(hashMap);
+
+      for(int x = 0; x<input.length; x++){
+         for(int y = 0; y<input[x].length(); y++){
+            System.out.println(input[x].substring(0,y));
+            if(hashMap.containsKey(input[x].substring(0,y))){
+               System.out.println(false);
+            }else {
+               System.out.println(true);
+            }
+         }
+      }
+   }
+}
