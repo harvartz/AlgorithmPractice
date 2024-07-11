d939ab66a778dacec762f1fe198af8d95bf7524e
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2024-07-11 17:19:50 +0900

    [level 2] Title: H-Index, Time: 0.55 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,12 +4,18 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         
-        Arrays.sort(citations);
         
-        // 0, 1, 3, 5, 6
-        //answer = citations.length;
         
-        int max = 0;
+        Arrays.sort(citations); // 정렬 해주기
+        
+        // 정렬 => [0, 1, 3, 5, 6]
+        // 0번 이상 인용된 논문 갯수 : citations.length(=5)
+        // 1번 이상 인용된 논문 갯수 : citations.length(=5) - 1 = 4
+        // 3번 이상 인용된 논문 갯수 : citations.length(=5) - 2 = 3
+        // 5번 이상 인용된 논문 갯수 : citations.length(=5) - 3 = 2
+        // 6번 이상 인용된 논문 갯수 : citations.length(=5) - 4 = 1
+        
+        /*int max = 0;
         for(int i=0; i<citations.length; i++)
         {
             if(citations[i] >= (citations.length-i))
@@ -20,11 +26,16 @@ class Solution {
                     System.out.println(citations[i]+"/"+(citations.length-i));
                 }
             }
-        }
+        }*/
         
-        if(max != 0)
+        for(int i=0; i<citations.length; i++)
         {
-            answer = max;
+            int h = citations.length - i; // 논문 갯수
+            if(citations[i] >= h)
+            {
+                answer = h;
+                break;
+            }
         }
         
         return answer;
