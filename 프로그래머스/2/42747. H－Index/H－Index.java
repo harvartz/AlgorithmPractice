73ee061e4d9890a75682a3844fcaac6706f21364
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2024-07-31 11:24:46 +0900

    [level 2] Title: H-Index, Time: 0.95 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,39 +4,16 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         
-        
-        
-        Arrays.sort(citations); // 정렬 해주기
-        
-        // 정렬 => [0, 1, 3, 5, 6]
-        // 0번 이상 인용된 논문 갯수 : citations.length(=5)
-        // 1번 이상 인용된 논문 갯수 : citations.length(=5) - 1 = 4
-        // 3번 이상 인용된 논문 갯수 : citations.length(=5) - 2 = 3
-        // 5번 이상 인용된 논문 갯수 : citations.length(=5) - 3 = 2
-        // 6번 이상 인용된 논문 갯수 : citations.length(=5) - 4 = 1
-        
-        /*int max = 0;
-        for(int i=0; i<citations.length; i++)
-        {
-            if(citations[i] >= (citations.length-i))
-            {
-                if(max < citations.length-i)
-                {
-                    max = citations.length-i;
-                    System.out.println(citations[i]+"/"+(citations.length-i));
-                }
-            }
-        }*/
-        
-        for(int i=0; i<citations.length; i++)
-        {
-            int h = citations.length - i; // 논문 갯수
-            if(citations[i] >= h)
-            {
-                answer = h;
-                break;
-            }
-        }
+        Arrays.sort(citations);
+		
+		for(int i = 0; i < citations.length; i++) {
+			int h = citations.length - i; // 인용된 논문의 수
+			
+			if(citations[i] >= h) {
+				answer = h;
+				break;
+			}
+		}
         
         return answer;
     }
