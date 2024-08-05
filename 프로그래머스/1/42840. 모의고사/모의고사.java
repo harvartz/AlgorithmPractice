c8e6ab1bd58d15acd75b1ce999327ef112fafc5c
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2024-08-05 15:17:59 +0900

    [level 1] Title: 모의고사, Time: 1.77 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -4,11 +4,6 @@ import java.io.*;
 class Solution {
     public ArrayList<Integer> solution(int[] answers) {
         int[] answer = new int[3];
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = 0; // 초기화
-        }
-
-        // int형 배열 안에 ArrayList를 가지고 있는 자료구조
         int[][] counts = new int[3][];
         
         counts[0] = new int[]{1,2,3,4,5};
@@ -17,40 +12,33 @@ class Solution {
 
         for(int i = 0; i<3; i++){
             int z = 0;
-            // 찍는 방식, 문제 답 비교
             for(int j = 0; j < answers.length; j++){
-                // System.out.println( i+"번째 수포자, 현재 찍는 방식 값: " + counts[i][z]);
-                // System.out.println("인덱스 z: " + z);
-                // System.out.println("정답: "+ answers[j]);
                 if(counts[i][z] == answers[j]){
                     answer[i]+=1;
                 }
-                // System.out.println(i+ "번째 수포자 값: " + answer[i]);
-                // System.out.println();
-                if(z== counts[i].length-1){
+                if(z== counts[i].length-1){ // 규칙이 끝나면, 처음부터 다시 시작하기 위해서 변수 값을 0으로 변환해준다.
                         z = 0;
                 }else{
                     z++;
                 }
             }
         }
+        // 이렇게 하면 한줄로 간단하게 끝낼 수 있다.
+        // 모듈러 연산자를 사용하여 
+        // if(answer[i] == counts[i][z%5]) scores[i]++;
         
-        // 해당 최대값일 때 index + 1을 반환해야 한다.
         ArrayList<Integer> result = new ArrayList(); //인덱스를 담은 배열
         int max = Integer.MIN_VALUE;
-        int person = 0;
         // 최댓값을 찾는다.
-        // 최댓값과 같은 값이 있으면 추가로 더해준다.
+        // int max = Math.max(score[0], Math.max(score[1], score[2])); // 이런 코드도 존재한다.
         for(int i = 0; i<answer.length; i++){
             max = Math.max(max, answer[i]);
         }
-        
         for(int i = 0; i<answer.length;i++){
             if(max == answer[i]){
-                result.add(i+1);
+                result.add(i+1); // 사람을 찾는 것이기 때문에 인덱스 + 1을 해준다
             }
         }
         return result;
-        
     }
 }
