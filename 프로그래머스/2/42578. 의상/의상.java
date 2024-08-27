e8a57e31aded5255f3022287bf80bc2424530b85
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2024-08-27 10:47:05 +0900

    [level 2] Title: 의상, Time: 0.09 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,17 +1,22 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
+
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String,Integer> map= new HashMap<>();
-        for(String[] clothe : clothes){
-            String type = clothe[1];
-             map.put(type,map.getOrDefault(type,0)+1);
+         
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        
+        for(int i = 0; i<clothes.length; i++){
+                // 1번은 의상의 종류, 0번은 의상의 이름
+                hashMap.put(clothes[i][1], hashMap.getOrDefault(clothes[i][1], 0) + 1);
         }
-        int answer =1;
-        for(String key : map.keySet()){
-            answer *= (map.get(key)+1);
+        
+        Iterator<Integer> it = hashMap.values().iterator();
+        int answer = 1;
+        while(it.hasNext()){
+            answer *= it.next().intValue() + 1;
         }
-        amswer -=1;
-        return answer;
+        
+        return answer - 1;
     }
 }
