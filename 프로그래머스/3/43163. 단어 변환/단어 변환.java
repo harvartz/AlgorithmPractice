4d1dcec4e0ff33581518082b9b5566e0082eb0a8
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2024-09-01 15:11:54 +0900

    [level 3] Title: 단어 변환, Time: 0.33 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,68 +1,39 @@
-import java.util.*;
-
 class Solution {
+    int answer = 0;
+    boolean[] visited;
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-		
-		boolean [] visited = new boolean[words.length];
-		
-		ArrayDeque<String> q = new ArrayDeque<>();
-		
-		q.add(begin);
-		
-		boolean bFind = false;
-		while(!q.isEmpty())
-		{
-			int q_size = q.size();
-			
-			for(int size=0; size<q_size; size++)
-			{
-				String tmpTarget = q.poll();
-				
-				if(tmpTarget.equals(target))
-				{
-					bFind = true; // 변환 가능한 경우
-					break;
-				}
-				
-				// 한글자만 다른 항목들을 다 q에 넣어준다.
-				for(int i=0; i<words.length; i++)
-				{
-					// 한글자만 다른지 확인
-					int tmp_cnt = 0;
-					for(int j=0; j<words[i].length(); j++)
-					{
-						if(words[i].charAt(j) == tmpTarget.charAt(j))
-						{
-							tmp_cnt++;
-						}
-					}
-					
-					if(tmp_cnt >= tmpTarget.length()-1)
-					{
-						//System.out.println(words[i]);
-						if(visited[i] == false)
-						{
-							visited[i] = true;
-							q.add(words[i]);
-						}
-					}
-				}
-				
-			}
-			
-			answer++;
-		}
-		
-		if(bFind == false)
-		{
-			answer = 0;
-		}
-		else
-		{
-			answer -=1;
-		}
-		
-		return answer;
+        visited = new boolean[words.length];
+        dfs(begin, target, words, 0);
+        return answer;
+    }
+    
+    public void dfs(String word, String target, String[] words, int depth){
+        // 만약 바꾼 단어가 target과 일치하다? 그러면 바로 return
+        if(word.equals(target)) {
+            answer = depth;
+            return;
+        }
+        
+        
+        for(int i = 0; i < words.length; i++){
+            
+            if(visited[i]) continue;
+            if(isPossible(word, words[i])){
+                visited[i] = true;
+                dfs(words[i], target, words, depth + 1);
+                visited[i] = false;
+            }
+        }            
+    }
+    
+    public boolean isPossible(String now, String compare){
+        int count = 0;
+        for(int i = 0; i<now.length(); i++){
+            if(now.charAt(i) != compare.charAt(i)){
+                count++;
+            }
+        }
+        if(count == 1) return true;
+        else return false;
     }
 }
