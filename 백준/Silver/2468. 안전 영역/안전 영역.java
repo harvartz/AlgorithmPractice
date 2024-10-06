60e7ba626c487917fcab52eedeab8cd8160e89eb
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2024-10-06 13:52:30 +0900

    [Silver I] Title: 안전 영역, Time: 216 ms, Memory: 21220 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -1,155 +1,60 @@
+import java.io.*;
 import java.util.*;
 
-public class Main {
-	
-	static int N; // 배열 W,H
-	static int [][] map; // 안전영역 정보 담고 있는 2차원 배열
-	static boolean [][] visit; // 방문 확인
-	
-	static int min = Integer.MAX_VALUE; //입력받는 수 중 제일 작은 수
-	static int max = Integer.MIN_VALUE; //입력받는 수중 제일 큰 수
-	
-	static int [] di = {-1,1,0,0};
-	static int [] dj = {0,0,-1,1};
+class Main {
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int[][] map;
+    static boolean[][] visited;
+    static int n;
+    static int count = 0;
+    static int Max = 0; // 초기값을 0으로 설정
 
-	//static int res = 0;
-	
-	static void init_visit()
-	{
-		for(int i=0; i<N; i++)
-		{
-			for(int j=0; j<N; j++)
-			{
-				visit[i][j]=false;
-			}
-		}
-	}
-	
-	// dfs
-	static void dfs(int condition, int i, int j)
-	{
-		visit[i][j] = true;
-		
-		//System.out.println(map[i][j]);
-		
-		for(int d=0; d<4; d++)
-		{
-			int ni = i+di[d];
-			int nj = j+dj[d];
-			
-			if(ni >= 0 && ni <N && nj >=0 && nj <N)
-			{
-				if(visit[ni][nj] == false)
-				{
-					if(map[ni][nj] > condition)
-					{
-						dfs(condition,ni,nj);
-					}
-				}
-			}
-			
-		}
-		
-	}
-	
-	static void bfs(int condition, int i, int j)
-	{
-		ArrayDeque<int []> q = new ArrayDeque<>();
-		visit[i][j]=true;
-		q.offer(new int[] {i,j});
-		
-		
-		while(!q.isEmpty())
-		{
-			int [] cur = q.poll();
-			
-			i = cur[0];
-			j = cur[1];
-			
-			for(int d=0; d<4; d++)
-			{
-				int ni = i+di[d];
-				int nj = j+dj[d];
-				
-				if(ni >=0 && ni<N && nj >=0 && nj < N)
-				{
-					if(visit[ni][nj] == false)
-					{
-						if(map[ni][nj] > condition)
-						{
-							visit[ni][nj]=true;
-							q.offer(new int[] {ni,nj});
-						}
-					}
-				}
-			}
-		}
-	}
-	
-	
-	public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        // 제출 시 파일 입력 제거
+        // System.setIn(new FileInputStream("src/main/resources/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        int height = Integer.MIN_VALUE;
+        int minHeight = Integer.MAX_VALUE; // 실제 최소 높이로 설정
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                height = Math.max(height, map[i][j]);
+                minHeight = Math.min(minHeight, map[i][j]);
+            }
+        }
 
-		// 입력받기
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		
-		map = new int[N][N];
-		visit = new boolean[N][N];
-		
-		for(int i=0; i<N; i++)
-		{
-			for(int j=0; j<N; j++)
-			{
-				map[i][j] = sc.nextInt();
-				
-				min = Math.min(min, map[i][j]);
-				max = Math.max(max, map[i][j]);
-			}
-		}
-		
-		int count = 1; // 비가 오지 않았을 경우 
-		{
-			// 초기화 필요
-			
-			for(int condition = min; condition <= max; condition++)
-			{
-				int res_tmp = 0;
-				init_visit();
-				
-				for(int i=0; i<N; i++)
-				{
-					for(int j=0; j<N; j++)
-					{
-						if(visit[i][j]== false)
-						{
-							if(map[i][j] > condition)
-							{
-								bfs(condition,i,j);
-								res_tmp++;
-							}
-							
-						}
-					}
-				}
-				
-				if(count < res_tmp)
-				{
-					count = res_tmp;
-				}
-				
-			}
-		}
-		
-		
-		
-		System.out.println(count);
-		
-		/*for(int dept=min+1; dept<max; dept++)
-		{
-			
-				
-			// 잠기는 영역을 min+1 ~ max-1 까지 바꿔가면서 확인
-		}*/
-	}
+        // 물 높이를 0부터 최대 높이까지 고려
+        for (int i = 0; i <= height; i++) { // 0부터 시작
+            visited = new boolean[n][n];
+            count = 0;
+            for (int j = 0; j < n; j++) {
+                for (int z = 0; z < n; z++) {
+                    if (!visited[j][z] && map[j][z] > i) {
+                        count++;
+                        dfs(j, z, i);
+                    }
+                }
+            }
+            Max = Math.max(count, Max);
+        }
+        System.out.println(Max);
+    }
 
+    static void dfs(int x, int y, int level) {
+        visited[x][y] = true;
+        for (int i = 0; i < 4; i++) {
+            int d_x = x + dx[i];
+            int d_y = y + dy[i];
+            if (0 <= d_x && d_x < n && 0 <= d_y && d_y < n) {
+                if (!visited[d_x][d_y] && map[d_x][d_y] > level) {
+                    dfs(d_x, d_y, level);
+                }
+            }
+        }
+    }
 }
