cade92ba3628e95df705bd86ddb5c1dfb7835d7d
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2024-10-08 10:49:05 +0900

    Time: 3 ms (67.45%), Space: 44.6 MB (77.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -6,9 +6,10 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(target - nums[i])) {
+                // target- nums[i] key값에 대한 인덱스 , i 인덱스 (현재 순회중인)
                 return new int[]{map.get(target - nums[i]), i};
             }
-
+            // 그게 아니면 key, value를 넣는다.
             map.put(nums[i], i);
         }
 
