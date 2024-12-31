d115c1ed4b96246506d184d595a0f95f3cc07478
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2024-12-31 22:09:06 +0900

    [Silver I] Title: 안전 영역, Time: 212 ms, Memory: 18568 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -2,57 +2,55 @@ import java.io.*;
 import java.util.*;
 
 class Main {
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, 1, 0, -1};
     static int[][] map;
     static boolean[][] visited;
-    static int n;
-    static int count = 0;
-    static int Max = 0; // 초기값을 0으로 설정
+    static int safeArea;
+    static int nowArea;
+    static int[] dx = new int[]{-1, 0, 1, 0};
+    static int[] dy = new int[]{0, -1, 0, 1};
+    static int N;
 
     public static void main(String[] args) throws IOException {
-        // 제출 시 파일 입력 제거
-        // System.setIn(new FileInputStream("src/main/resources/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = null;
-        n = Integer.parseInt(br.readLine());
-        map = new int[n][n];
-        int height = Integer.MIN_VALUE;
-        int minHeight = Integer.MAX_VALUE; // 실제 최소 높이로 설정
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        int maxHeight = Integer.MIN_VALUE;
+        int minHeight = Integer.MAX_VALUE;
+        safeArea = 1;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                height = Math.max(height, map[i][j]);
+                maxHeight = Math.max(maxHeight, map[i][j]);
                 minHeight = Math.min(minHeight, map[i][j]);
             }
         }
 
-        // 물 높이를 0부터 최대 높이까지 고려
-        for (int i = 0; i <= height; i++) { // 0부터 시작
-            visited = new boolean[n][n];
-            count = 0;
-            for (int j = 0; j < n; j++) {
-                for (int z = 0; z < n; z++) {
-                    if (!visited[j][z] && map[j][z] > i) {
-                        count++;
-                        dfs(j, z, i);
+        for (int i = minHeight; i <= maxHeight; i++) {
+            int nowArea = 0;
+            visited = new boolean[N][N];
+            for (int x = 0; x < N; x++) {
+                for (int y = 0; y < N; y++) {
+                    if (!visited[x][y] && map[x][y] > i) {
+                        dfs(x, y, i);
+                        nowArea++;
                     }
                 }
             }
-            Max = Math.max(count, Max);
+            safeArea = Math.max(nowArea, safeArea);
         }
-        System.out.println(Max);
+        System.out.println(safeArea);
     }
 
-    static void dfs(int x, int y, int level) {
+    static void dfs(int x, int y, int nowHeight) {
         visited[x][y] = true;
         for (int i = 0; i < 4; i++) {
-            int d_x = x + dx[i];
-            int d_y = y + dy[i];
-            if (0 <= d_x && d_x < n && 0 <= d_y && d_y < n) {
-                if (!visited[d_x][d_y] && map[d_x][d_y] > level) {
-                    dfs(d_x, d_y, level);
+            int d_x = dx[i] + x;
+            int d_y = dy[i] + y;
+
+            if (0 <= d_x && d_x < N && 0 <= d_y && d_y < N) {
+                if (!visited[d_x][d_y] && map[d_x][d_y] > nowHeight) {
+                    dfs(d_x, d_y, nowHeight);
                 }
             }
         }
