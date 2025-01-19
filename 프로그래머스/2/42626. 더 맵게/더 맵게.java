4cf04b4dae9158f6ba2fbc69d5cca8124294d13f
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-01-19 22:55:20 +0900

    [level 2] Title: 더 맵게, Time: 1355.01 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,34 +1,23 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
-    
     public int solution(int[] scoville, int K) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i : scoville){
-            pq.add(i);
-        }
-        // 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞음
+        
+        PriorityQueue<Integer> que = new PriorityQueue<>();
         int answer = 0;
-        while(!findAll(pq, K)){
-            if(pq.size() == 1) return -1;
-            
-            int first = pq.poll();
-            int second = pq.poll();
-            int mixed = first + second * 2;
-            pq.offer(mixed);
+        
+        for(int x = 0; x<scoville.length; x++){
+            que.add(scoville[x]);
+        }
+        
+        while(que.peek() < K){
+            if(que.size()==1){
+                return -1;
+            }
+            que.add(que.poll() + que.poll()*2);
             answer++;
         }
-        return answer;
-    }
-    
-    public static boolean findAll(PriorityQueue<Integer> pq, int K){
         
-        for(int i : pq){
-            if(i < K) {
-                return false;
-            }
-        }
-        return true;
+        return answer;
     }
 }
