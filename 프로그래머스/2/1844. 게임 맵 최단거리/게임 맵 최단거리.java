aec8ab7b312dd2961cf484695ce83b792aeb9bc8
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-01-23 10:52:18 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.27 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,58 +1,47 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
-    int n;
-    int m;
-    int count = 0;
+    int n = 0;
+    int m = 0;
+    int answer = 0;
+    int[] dx = {-1, 0, 1,  0};
+    int[] dy = {0 , 1, 0, -1};
     int[][] dist;
     Queue<int[]> qu = new LinkedList<>();
-    int[] dx = {-1, 0, 1, 0};
-    int[] dy = {0, -1, 0, 1};
     
     public int solution(int[][] maps) {
-        // 1. 변수 초기 세팅
+        
         n = maps.length;
         m = maps[0].length;
-        System.out.println("n: " + n +", m:" + m);
+        
         dist = new int[n][m];
+        dist[0][0] = 1;
+        qu.offer(new int[]{0,0});
+        bfs(maps);
         
-        dfs(0, 0, maps);
-        
-        // for(int x = 0; x<n; x++){
-        //     for(int y = 0; y<m; y++){
-        //         System.out.print(dist[x][y] + "  ");
-        //     }
-        //     System.out.println();
-        // }
-        
-        if(dist[n-1][m-1] == 0) return -1;
-        int answer = dist[n-1][m-1];
-        return answer;
+        if(dist[n-1][m-1] > 0){
+            return dist[n-1][m-1];
+        } else {
+            return -1;
+        }
     }
     
-    public void dfs(int x, int y, int[][] maps){
-        dist[x][y] = 1;
-        qu.offer(new int[]{x, y});
-        
+    public void bfs(int[][] maps){
         while(!qu.isEmpty()){
             int[] now = qu.poll();
-            for(int i = 0; i < 4; i++){
+            
+            for(int i = 0; i<4; i++){
                 int d_x = now[0] + dx[i];
                 int d_y = now[1] + dy[i];
                 
                 if(0 <= d_x && d_x < n && 0 <= d_y && d_y < m){
                     if(dist[d_x][d_y] == 0 && maps[d_x][d_y] == 1){
                         dist[d_x][d_y] = dist[now[0]][now[1]] + 1;
-                        if(d_x == n-1 && d_y == m-1 ) {
-                            return;
-                        }
-                        int[] in = new int[]{d_x, d_y};
-                        qu.offer(in);
-                    }
+                        qu.offer(new int[]{d_x, d_y});
+                    } 
                 }
             }
         }
-        
-        
-   }
+    }
 }
