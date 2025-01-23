04d3d28acb188e44625978a96d14268d8480b6c3
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-01-23 14:16:30 +0900

    [level 3] Title: 네트워크, Time: 0.27 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,12 +1,10 @@
-import java.io.*;
-import java.util.*;
-
 class Solution {
-    int count = 0;
-    boolean[] visited; // 노드에 방문했는지 안했는지 확인하는 배열
+    boolean[] visited;
     public int solution(int n, int[][] computers) {
+        
         visited = new boolean[n];
-
+        int count = 0;
+        
         for(int i = 0; i<n; i++){
             if(!visited[i]){
                 visited[i] = true;
@@ -17,10 +15,10 @@ class Solution {
         return count;
     }
     
-    public void dfs(int start, int size, int[][] computers){
-        for(int i = 0; i<size; i++){
-            // 연결되어있는지 && 다음 탐색하려는 노드가 방문안했다면
-            if(computers[start][i] == 1 && !visited[i]){
+    public void dfs(int now, int size, int[][] computers){
+        
+        for(int i = 0; i < size; i++){
+            if(computers[now][i] == 1 && !visited[i]){
                 visited[i] = true;
                 dfs(i, size, computers);
             }
