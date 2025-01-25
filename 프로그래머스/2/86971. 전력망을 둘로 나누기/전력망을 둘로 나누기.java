6df95812f57303f82a509b3191e2a268ebea0fc7
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-01-25 23:33:46 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 21.34 ms, Memory: 94.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,53 +1,62 @@
+import java.util.*;
+import java.io.*;
+
 class Solution {
-    int[][] graph;
     boolean[] visited;
-    int aCount;
-
+    int[][] graph;
+    int count = 0;
+    int answer = Integer.MAX_VALUE;
+    int size;
+    
     public int solution(int n, int[][] wires) {
+        size = n;
+        visited = new boolean[n];
         graph = new int[n][n];
-        int answer = Integer.MAX_VALUE;
         
-        // 그래프 초기화
-        for (int i = 0; i < wires.length; i++) {
-            int start = wires[i][0] - 1;
-            int end = wires[i][1] - 1;
-            graph[start][end] = 1;
-            graph[end][start] = 1;
+        for(int i = 0; i < wires.length; i++){
+            for(int j = 0; j < 2; j++){
+                int aNode = wires[i][0] - 1;
+                int bNode = wires[i][1] - 1;
+                
+                graph[aNode][bNode] = 1;
+                graph[bNode][aNode] = 1;
+            }
         }
-
-        // 모든 간선을 하나씩 끊어서 시도
-        for (int i = 0; i < wires.length; i++) {
-            int start = wires[i][0] - 1;
-            int end = wires[i][1] - 1;
-            graph[start][end] = 0;  // 간선을 끊음
-            graph[end][start] = 0;
-
+        
+        for(int i = 0; i < wires.length; i++){
+            int aNode = wires[i][0] - 1;
+            int bNode = wires[i][1] - 1;
+            graph[aNode][bNode] = 0;
+            graph[bNode][aNode] = 0;
+            System.out.println("aNode: " + aNode + ", bNode:" + bNode);
+            
+            // 0번 노드부터 시작
+            dfs(0);
+            
+            // 순회하기 - 하나의 그래프만 구하면 전체에서 빼면 된다.
+            int a = Math.abs((n-count) - count);
+            answer = Math.min(a, answer);
+            
+            count = 0;
             visited = new boolean[n];
-            aCount = 0;
-            
-            // 0번 노드부터 시작해서 탐색 (연결된 노드 개수 세기)
-            dfs(0, n);
-            int bCount = n - aCount; // 다른 그룹의 노드 개수
-            
-            // 두 그룹의 차이를 최소화
-            answer = Math.min(answer, Math.abs(aCount - bCount));
-
-            // 끊은 간선 복구
-            graph[start][end] = 1;
-            graph[end][start] = 1;
+            graph[aNode][bNode] = 1;
+            graph[bNode][aNode] = 1;
+        }
+        
+        if(answer == Integer.MAX_VALUE){
+            return -1;
         }
         return answer;
     }
-
-    // DFS로 연결된 노드 개수 세기
-    public void dfs(int now, int n) {
-        visited[now] = true;
-        aCount++;
+    
+    public void dfs(int nowNode){
+        visited[nowNode] = true;
+        count++;
         
-        for (int i = 0; i < n; i++) {
-            if (!visited[i] && graph[now][i] == 1) {
-                dfs(i, n);
-            }            
+        for(int j = 0; j < size; j++){
+            if(!visited[j] && graph[nowNode][j] == 1){
+                dfs(j);
+            }
         }
     }
 }
