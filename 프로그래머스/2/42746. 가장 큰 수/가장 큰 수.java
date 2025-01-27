0790bfe79532b4329a9903a8ba5b16b4dc211c48
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-01-27 17:41:02 +0900

    [level 2] Title: 가장 큰 수, Time: 3306.18 ms, Memory: 396 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,96 +1,29 @@
 import java.util.*;
-import java.io.*;
-
-class Solution {
-    
-    public String solution(int[] numbers) {
-        
-        String answer = "";
-        
-        // 정렬 1차로 해주기...
-        String [] num = new String[numbers.length];
-        
-        for(int i=0; i<numbers.length; i++)
-        {
-            num[i] = String.valueOf(numbers[i]);
-        }
-        
-        //Arrays.sort(num, Collections.reverseOrder());
-        Arrays.sort(num, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-        
-        /*for(int i=0; i<numbers.length; i++)
-        {
-            
-            System.out.print(numbers[i] + " ");
-        }
-        System.out.println();*/
-        
-        // 2차 정렬 => 818,81 or 816,81
-        for(int i=0; i<num.length-1; i++)
-        {
-            String tmp1 = num[i]+num[i+1];
-            String tmp2 = num[i+1]+num[i];
-                
-            if(tmp1.compareTo(tmp2) <= 0)
-            {
-                // 303, 330 => 순서를 변경해야하는 경우
-                String tmp3 = num[i];
-                num[i] = num[i+1];
-                num[i+1] = tmp3;
-                    
-                //System.out.println("1."+tmp1 + "/" + tmp2);
-            }   
-            
-            answer+=num[i];
-        }
-        
-        answer+=num[num.length-1];
-        
-        //answer+=num[num.length-1];
-        
-
-       // 000인 경우 => 0으로 나와야 한다
-       if (answer.charAt(0) == '0')
-       {
-           answer = "0";
-       }
-        
-       return answer;
-    }
-}
-
-
-
-
-
-
-/*import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
         
-        String [] tmp_arr = new String[numbers.length];
+        String[] result = new String[numbers.length];
         
-        for(int i =0; i<numbers.length; i++)
-        {
-            tmp_arr[i] = Integer.toString(numbers[i]);
+        for(int x=0; x<numbers.length;x++){
+            result[x] = String.valueOf(numbers[x]);
         }
         
-        Arrays.sort(tmp_arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        // 여기가 핵심.
+        // 두 수를 합쳤을 때(문자열 합치기) 
+        // compareTo() 함수
+        Arrays.sort(result, (o1, o2) -> {
+            return (o2 + o1).compareTo(o1 + o2);
+        });
+        
+        if(result[0].equals("0")){
+            return "0";   
+        }
         
         String answer = "";
-        if (tmp_arr[0].equals("0") == false) 
-        {
-            for(int i=0; i<numbers.length; i++)
-            {
-                answer+=tmp_arr[i];
-            }
+        for(String a : result){
+            answer += a;
         }
-        else
-        {
-            answer = "0";
-        }
-        
         return answer;
     }
-}*/
+}
