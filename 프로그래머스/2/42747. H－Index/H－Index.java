edf7434fce46a7f55e7cc7360eaa87140110860f
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-01-28 21:00:40 +0900

    [level 2] Title: H-Index, Time: 1.57 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,10 +4,8 @@ class Solution {
     public int solution(int[] citations) {
         int answer = 0;
         
-        // 정렬을 진행한다.
         Arrays.sort(citations);
 		
-        // 0번째 인덱스부터 
 		for(int i = 0; i < citations.length; i++) {
 			int h = citations.length - i; // 인용된 논문의 수
 			if(citations[i] >= h) {
