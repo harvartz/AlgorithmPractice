90c91de47028b81fb3f75ab8014fd7c89fefa59b
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-01-30 22:09:29 +0900

    [level 2] Title: 전화번호 목록, Time: 308.84 ms, Memory: 268 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,33 +3,20 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         
-        boolean answer = true;
+        HashMap<String, Integer> hashMap = new HashMap<>();
         
-        Arrays.sort(phone_book);
-        
-        for(int i=0; i<phone_book.length-1; i++)
-        {
-            int ori_len = phone_book[i].length();
-            int next_len = phone_book[i+1].length();
-            
-            if(ori_len <= next_len)
-            {
-                // 접두어인지 확인 필요.
-                String tmp = phone_book[i+1].substring(0,ori_len);
-                //System.out.println(tmp);
-                
-                if(phone_book[i].compareTo(tmp) == 0)
-                {
-                    //System.out.println("접두어 존재");
-                    answer = false;
-                    break;
-                }
-            }
-            
+        for(String x : phone_book){
+            hashMap.put(x, 1);
         }
         
-        
-        
-        return answer;
+        // containsKey 활용하기
+        for(int x = 0; x<phone_book.length; x++){
+            for(int y = 1; y<phone_book[x].length(); y++){
+                if(hashMap.containsKey(phone_book[x].substring(0,y))){
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
