8d879b79c4d320cbd003d2a1786d111ec31a08fa
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-02-02 23:48:36 +0900

    [level 2] Title: 할인 행사, Time: 21.75 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
+        for (int i = 0; i < discount.length - 9; i++) {
+            int idx = 0;
+            for (int j = idx; j < want.length; j++) {
+                int num = number[j];
+                for (int k = i; k < i + 10; k++) {
+                    if (discount[k].equals(want[j])) num--;
+                }
+                if (num > 0) {
+                     break;
+                }
+                idx++; // 다음 상품을 위해 idx 증가
+            }
+            if (idx == want.length) answer++;
+        }
+        return answer;
+    }
+}
