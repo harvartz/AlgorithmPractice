7d1d04e611090cecec254c70199152d32306bbcb
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-02-05 23:55:08 +0900

    [Silver III] Title: 소수 구하기, Time: 612 ms, Memory: 30148 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,28 +1,32 @@
-import java.util.Scanner;
+import java.io.*;
+
 public class Main {
     public static boolean[] prime;
-    public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
 
-        int M = sc.nextInt();
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        prime = new boolean[N+1];
+        String[] inputs = br.readLine().split(" ");
+        int M = Integer.parseInt(inputs[0]);
+        int N = Integer.parseInt(inputs[1]);
 
+        prime = new boolean[N + 1];
         get_prime();
 
-        for(int x = M; x<=N; x++){
-            if(!prime[x]) System.out.println(x);
+        for (int x = M; x <= N; x++) {
+            if (!prime[x]) {
+                System.out.println(x);
+            }
         }
 
     }
 
-    public static void get_prime(){
-        prime[0] = prime[1] = true;
+    public static void get_prime() {
+        prime[0] = prime[1] = true; // 0과 1은 소수가 아님
 
-        for(int x = 2; x< Math.sqrt(prime.length); x++){
-            if(prime[x]) continue;
-            for(int y = x*x; y< prime.length; y+=x){
+        for (int x = 2; x <= Math.sqrt(prime.length); x++) {
+            if (prime[x]) continue;
+            for (int y = x * x; y < prime.length; y += x) {
                 prime[y] = true;
             }
         }
