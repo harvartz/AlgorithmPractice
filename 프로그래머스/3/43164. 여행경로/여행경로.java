e33262c40b6ce5d29b288a1725c2345f242fb6a7
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-02-06 16:08:21 +0900

    [level 3] Title: 여행경로, Time: 110.83 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,13 +1,6 @@
 import java.util.*;
 import java.io.*;
 
-// 특징 1. 주어진 모든 항공권을 이용해야 한다.
-// 특징 2. 만일 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로를 return 한다
-// 방법 1. 정렬을 먼저 하면 처리가 되는가?
-// 방법 2. 도착지를 뽑을 때, 그때마다 다른 도착지의 배열을 정렬해서 뽑으면 되는건가
-
-// ICN이 맨 앞에 안올수가 있나?
-
 
 class Solution {
     boolean[] visited;
@@ -25,7 +18,6 @@ class Solution {
     }
     
     public void dfs(int depth, String start, String route, String[][] tickets){
-        // 1. 탈출 조건
         if(depth == tickets.length){
             result.add(route);
             return;
