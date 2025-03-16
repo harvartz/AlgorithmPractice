939229dea33d32921735824804969680615b4035
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-03-17 02:35:25 +0900

    [Silver I] Title: 경로 찾기, Time: 492 ms, Memory: 31536 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,53 +1,57 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
-
-    static int n;
-    static boolean[][] am = new boolean[102][102];
-    static boolean[] vis = new boolean[102];
-
-    static boolean dfs(int st, int tar) {
-        for (int i = 0; i <= n + 1; i++) {
-            vis[i] = false;
-        }
-
-        Stack<Integer> stk = new Stack<>();
-        stk.push(st);
-
-        while (!stk.empty()) {
-            int cur = stk.pop();
-
-            if (vis[cur]) continue;
-            if (cur != st) vis[cur] = true;
-
-            for (int nxt = 0; nxt < n; nxt++) {
-                if (vis[nxt]) continue;
-                if (am[cur][nxt]) {
-                    if (nxt == tar) return true;
-                    stk.push(nxt);
-                }
-            }
-        }
-        return false;
-    }
+    static int N;
+    static int[][] graph;
+    static int[][] result;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();  // 정점 개수
+        graph = new int[N][N];
+        result = new int[N][N];
 
-        n = sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int val = sc.nextInt();
-                am[i][j] = (val == 1);
+        // 그래프 입력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                graph[i][j] = sc.nextInt();
             }
         }
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print((dfs(i, j) ? "1 " : "0 "));
+        // 모든 정점에 대해 BFS 수행
+        for (int i = 0; i < N; i++) {
+            bfs(i);
+        }
+
+        // 결과 출력
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(result[i][j] + " ");
             }
             System.out.println();
+        }
+
+        sc.close();
+    }
+
+    // BFS를 사용하여 i번 정점에서 도달 가능한 노드를 찾음
+    static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[N];
+
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+
+            for (int next = 0; next < N; next++) {
+                // 갈 수 있는 길이 있고, 방문하지 않았다면 탐색
+                if (graph[current][next] == 1 && !visited[next]) {
+                    visited[next] = true;
+                    result[start][next] = 1; // 도달 가능 표시
+                    queue.add(next);
+                }
+            }
         }
     }
 }
