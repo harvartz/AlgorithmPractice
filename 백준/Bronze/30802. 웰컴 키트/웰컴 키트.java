b01f327762978ac1e17e23af09e8f72f550251cf
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-03-17 19:45:53 +0900

    [Bronze III] Title: 웰컴 키트, Time: 120 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -6,17 +6,14 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        // 참가자 수 입력
         int N = Integer.parseInt(br.readLine());
 
-        // 티셔츠 사이즈별 신청자 수 입력
         st = new StringTokenizer(br.readLine(), " ");
-        int[] sizes = new int[6]; // S, M, L, XL, XXL, XXXL
+        int[] sizes = new int[6]; 
         for (int i = 0; i < 6; i++) {
             sizes[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 묶음 단위 입력
         st = new StringTokenizer(br.readLine(), " ");
         int A = Integer.parseInt(st.nextToken()); // 티셔츠 묶음 크기
         int B = Integer.parseInt(st.nextToken()); // 펜 묶음 크기
@@ -24,7 +21,11 @@ class Main {
         // (1) 티셔츠 묶음 개수 계산 (올림)
         int tshirtBundles = 0;
         for (int i = 0; i < 6; i++) {
-            tshirtBundles += (sizes[i] + A - 1) / A; // 올림 계산
+            // A-1을 해야지 A의 배수를 넘어가는 경우, 다음 몫으로 자동으로 넘어간다고 함
+            // A로 딱 나눠떨어지는 경우에도 한 묶음을 더 주문하는 오류가 발생함!
+            // -> 이런 생각은 도데체 어떻게 해야하는거야
+            // EX) 10 /5 = 2인데, 10 + 5 / 5 =3으로 하나를 더 주문하는 오류가 생겨버림
+            tshirtBundles += (sizes[i] + A - 1) / A;
         }
         System.out.println(tshirtBundles);
 
