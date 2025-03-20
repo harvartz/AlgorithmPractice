95e18b257cb9c9707ccb8ea74520153bd6c8dd75
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-03-20 23:34:42 +0900

    [Silver II] Title: 스택 수열, Time: 304 ms, Memory: 27992 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
  
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -19,6 +17,7 @@ public class Main {
 			
 			int value = Integer.parseInt(br.readLine());
 			
+            // 처음보다 값이 더 커버리면
 			if(value > start) {
 				for(int i = start + 1; i <= value; i++) {
 					stack.push(i);
@@ -27,6 +26,7 @@ public class Main {
 				start = value; 	
 			}
 			
+            // 뽑은 값이 value 와 다를떄
 			else if(stack.peek() != value) {
 				System.out.println("NO");
 				return;	
