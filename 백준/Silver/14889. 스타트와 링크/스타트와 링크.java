c502b4340073cd6938607bd65964ee8bc015362d
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-12-07 21:14:56 +0900

    [Silver I] Title: 스타트와 링크, Time: 280 ms, Memory: 31096 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,76 +1,79 @@
 import java.io.*;
 import java.util.*;
 
-class Main {
-
+public class Main {
     static int N;
-    static int[][] arr;
-    static int[] b; // A팀의 선수들 저장
-    static int min = Integer.MAX_VALUE; // 최솟값을 구해야 하므로 Integer.MAX_VALUE로 설정
+    static int[][] map;
+    static boolean[] visited;
+    static int[] checked;
+    static int min = Integer.MAX_VALUE;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void diff() {
+        int teamStart = 0;
+        int teamLink = 0;
 
-        N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-        b = new int[N / 2]; // A팀은 N/2명의 선수로 구성됨
-
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < checked.length - 1; i++) {
+            for (int j = i + 1; j < checked.length; j++) {
+                int a = checked[i];
+                int b = checked[j];
+                teamStart += map[a][b] + map[b][a];
             }
         }
 
-        // 조합으로 A팀 선수들을 선택
-        recur(0, 0);
-        System.out.println(min);
+        int[] other = new int[checked.length];
+        int idx = 0;
+        outer:
+        for (int i = 0; i < N; i++) {
+            for (int c : checked) {
+                if (c == i) {
+                    continue outer;
+                }
+            }
+            other[idx++] = i;
+        }
+
+        for (int i = 0; i < other.length - 1; i++) {
+            for (int j = i + 1; j < other.length; j++) {
+                int a = other[i];
+                int b = other[j];
+                teamLink += map[a][b] + map[b][a];
+            }
+        }
+
+        int val = Math.abs(teamStart - teamLink);
+        min = Math.min(val, min);
     }
 
-    // 조합을 이용해 A팀(N/2명)을 구성하는 함수
-    static void recur(int start, int depth) {
-        if (depth == N / 2) {
+    // start 변수는 무엇인가?
+    // 문제에서 조합이라는 것을 어떻게 찾아내는가?
+
+    public static void perm(int start, int level) {
+        if (level == N / 2) {
             diff();
             return;
         }
-
         for (int i = start; i < N; i++) {
-            b[depth] = i; // A팀의 선수 추가
-            recur(i + 1, depth + 1);
+            checked[level] = i;
+            perm(i + 1, level + 1);
         }
     }
 
-    // A팀과 B팀의 차이를 계산하는 함수
-    static void diff() {
-        boolean[] isATeam = new boolean[N]; // A팀인지 여부를 저장하는 배열
+    public static void main(String[] args) throws Exception {
 
-        // A팀의 선수들을 표시
-        for (int i = 0; i < N / 2; i++) {
-            isATeam[b[i]] = true;
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int team_start = 0, team_link = 0;
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        checked = new int[N / 2];
 
-        for (int i = 0; i < N - 1; i++) {
-            for (int j = i + 1; j < N; j++) {
-                // A팀의 경우
-                if (isATeam[i] && isATeam[j]) {
-                    team_start += arr[i][j] + arr[j][i];
-                }
-                // B팀의 경우 (A팀이 아닌 경우)
-                else if (!isATeam[i] && !isATeam[j]) {
-                    team_link += arr[i][j] + arr[j][i];
-                }
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        int val = Math.abs(team_start - team_link);
-
-        if (val == 0) {
-            System.out.println(val);
-            System.exit(0);
-        }
-
-        min = Math.min(min, val);
+        perm(0, 0);
+        System.out.println(min);
     }
 }
