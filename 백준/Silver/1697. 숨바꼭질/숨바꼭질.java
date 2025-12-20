127b6f22733fe6ad9753aa47cb67d73ec39ed081
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-12-20 11:20:28 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 23492 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,37 +1,42 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int[] visited;
+class Main {
+
+    static int N, K;
+    static Queue<Integer> qu = new LinkedList();
+    static boolean[] visited = new boolean[100001];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> qu = new LinkedList<>();
-        visited = new int[100001];
+        qu.offer(N);
 
-        qu.offer(n);
         int level = 0;
         while (!qu.isEmpty()) {
-            int len = qu.size();
-            for (int i = 0; i < len; i++) {
-                int result = qu.poll();
-                int[] move = {result - 1, result + 1, result * 2};
-                visited[result] = 1;
+            int length = qu.size();
+            for (int i = 0; i < length; i++) {
+                int now = qu.poll();
+                visited[now] = true;
 
-                if (result == m) {
+                if (now == K) {
                     System.out.println(level);
                     return;
                 }
 
-                for (int x = 0; x < move.length; x++) {
-                    if (move[x] < 0 || move[x] > 100000) continue;
-                    if (visited[move[x]] == 1) continue;
-                    qu.offer(move[x]);
+                int[] move = { now + 1, now - 1, now * 2 };
+
+                for (int j = 0; j < 3; j++) {
+                    if (move[j] < 0 || move[j] > 100000)
+                        continue;
+                    if (visited[move[j]])
+                        continue;
+                    visited[move[j]] = true;
+                    qu.offer(move[j]);
                 }
             }
             level++;
