42fa578061cf4b3d8e03bb4a80d5fe25b78567a0
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-12-25 16:45:21 +0900

    [Gold V] Title: 토마토, Time: 584 ms, Memory: 120304 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,77 +1,81 @@
 import java.io.*;
 import java.util.*;
 
+// 토마토 문제
+// 하나의 토마토에 (4방향)인접한 토마토는 익게 됨
+// 안익은 토마토가 최소 몇일이 지나야 모든 토마토가 익게 될까? 
+
 class Main {
 
+    static int N, M;
     static int[][] map;
-    static int M;
-    static int N;
-    static Queue<Pair> qu = new LinkedList<>();
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
-    static int count = 0;
+    static int[][] time;
+
+    static Queue<int[]> tomato = new LinkedList<>();
+    static Queue<int[]> qu = new LinkedList<>();
+
+    static int[] dx = { 0, 1, 0, -1 };
+    static int[] dy = { 1, 0, -1, 0 };
+
+    public static void bfs() {
+
+        while (!qu.isEmpty()) {
+            int[] now = qu.poll();
+
+            for (int i = 0; i < 4; i++) {
+                int d_x = now[0] + dx[i];
+                int d_y = now[1] + dy[i];
+
+                if (0 <= d_x && d_x < M && 0 <= d_y && d_y < N) {
+                    if (map[d_x][d_y] == 0) {
+                        map[d_x][d_y] = map[now[0]][now[1]] + 1;
+                        qu.offer(new int[] { d_x, d_y });
+                    }
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
+        map = new int[M][N];
 
-        qu = new LinkedList<>();
-
-        for (int x = 0; x < N; x++) {
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int y = 0; y < M; y++) {
-                map[x][y] = Integer.parseInt(st.nextToken());
-                if (map[x][y] == 1) qu.offer(new Pair(x, y));
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+
+                if (map[i][j] == 1) {
+                    qu.offer(new int[] { i, j });
+                }
             }
         }
 
         bfs();
-
-        int ans = 0;
-        for (int x = 0; x < N; x++) {
-            for (int y = 0; y < M; y++) {
-                if (map[x][y] == 0) {
-                    ans = -1;
-                    System.out.println(ans);
-                    return;
-                }
-                ans = Math.max(ans, map[x][y]);
-            }
-        }
-        System.out.println(ans - 1);
-    }
-
-    static void bfs() {
-
-        while (!qu.isEmpty()) {
-            Pair p = qu.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int d_x = p.x + dx[i];
-                int d_y = p.y + dy[i];
-
-                if (d_x < 0 || d_x >= N || d_y < 0 || d_y >= M) continue;
-                if (map[d_x][d_y] == 0) {
-                    count++;
-                    map[d_x][d_y] = map[p.x][p.y] + 1;
-                    qu.offer(new Pair(d_x, d_y));
-                }
-            }
+        int answer = Integer.MAX_VALUE;
+        answer = findTime();
+        if (answer == -1) {
+            System.out.println(-1);
+        } else {
+            System.out.println(answer - 1);
         }
     }
 
-    static class Pair {
-        int x;
-        int y;
-
-        public Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
+    public static int findTime() {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (map[i][j] == 0) {
+                    return -1;
+                }
+                max = Math.max(max, map[i][j]);
+            }
         }
+        return max;
     }
 }
-
