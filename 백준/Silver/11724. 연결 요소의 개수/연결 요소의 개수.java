85195d7194ceb8314991e5b9efbd0919132832f3
Yang Seok Jin <62278686+harvartz@users.noreply.github.com>
2025-12-30 21:52:45 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 472 ms, Memory: 116952 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,51 +1,50 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
 
     static int N, M;
+    static int[][] graph;
     static boolean[] visited;
-    static int[][] map;
-    static int count = 0;
-    // 정점 N개, 간선 M개
-    // 둘째 줄부터 M개의 줄에 간선의 양 끝점 u와 v가 주어진다.
-    // 간선은 한 번만 주어진다.
 
-    public static void dfs(int start){
-        
-        for(int i = 0; i<N; i++){
-            if(map[start][i] == 1 && !visited[i]){
-                visited[i] = true;
+    public static void dfs(int node) {
+        visited[node] = true;
+
+        for (int i = 0; i < N; i++) {
+            if (graph[node][i] == 1 && !visited[i]) {
                 dfs(i);
             }
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
-        map = new int[N][N];
+        graph = new int[N][N];
         visited = new boolean[N];
+
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken()) - 1;
             int end = Integer.parseInt(st.nextToken()) - 1;
 
-            map[start][end] = 1;
-            map[end][start] = 1;
+            graph[start][end] = 1;
+            graph[end][start] = 1;
         }
 
-        for(int i = 0; i<N; i++){
-            if(!visited[i]){
-                visited[i] = true;
+        int count = 0;
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
                 dfs(i);
                 count++;
             }
         }
+
         System.out.println(count);
+
     }
 }
